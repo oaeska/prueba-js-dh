d66c3cf291ba2bb168a603e86edcc3759925a336
oaeska <oaeska@gmail.com>
2021-05-17 23:46:37 -0500

    final

--- app.js
+++ app.js
@@ -6,17 +6,25 @@
            * pendiente
          )
 */
+let fs = require('fs');
 
 exports.leerTareas = function () {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
+    return JSON.parse(fs.readFileSync('./tareas.json'));
+
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+        const tareas = JSON.parse(fs.readFileSync('./tareas.json'));
+        tareas.push(tarea);
+        fs.writeFileSync('./tareas.json', JSON.stringify(tareas))
+        return tareas;
+
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +32,6 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+        const tareas = JSON.parse(fs.readFileSync('./tareas.json'));
+        return tareas.filter(item => item.status == estado);
 }
